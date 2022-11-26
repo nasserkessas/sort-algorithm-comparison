9573111a813359ee766a84bc7278406286784df9
Nasser <nasser.kessas@outlook.com>
2022-11-26 21:40:53 +1000

    Removed function line spacing

--- main/python/insertion_sort.py
+++ main/python/insertion_sort.py
@@ -3,11 +3,8 @@ from random import shuffle
 ARRAY_LENGTH = 100
 
 def insertion_sort(array):
- 
     for i in range(1, ARRAY_LENGTH):
- 
         key = array[i]
- 
         j = i-1
         while j >= 0 and key < array[j]:
                 array[j + 1] = array[j]
