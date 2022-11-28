cde3721cdc49d895e711167625c079d8361c8618
Nasser <nasser.kessas@outlook.com>
2022-11-28 10:20:46 +1000

    Added merge sort algorithm

---
+++ main/python/merge_sort.py
@@ -0,0 +1,44 @@
+from random import shuffle
+
+ARRAY_LENGTH = 10
+
+def mergeSort(array):
+    if len(array) > 1:
+        i = j = k = 0
+
+        mid = len(array)//2
+        L = array[:mid]
+        R = array[mid:]
+        mergeSort(L)
+        mergeSort(R)
+        
+ 
+        while i < len(L) and j < len(R):
+            if L[i] <= R[j]:
+                array[k] = L[i]
+                i += 1
+            else:
+                array[k] = R[j]
+                j += 1
+            k += 1
+ 
+        while i < len(L):
+            array[k] = L[i]
+            i += 1
+            k += 1
+ 
+        while j < len(R):
+            array[k] = R[j]
+            j += 1
+            k += 1
+
+array = []
+
+for n in range(ARRAY_LENGTH):
+    array.append(n+1)
+
+shuffle(array)
+
+mergeSort(array)
+
+print(array)
