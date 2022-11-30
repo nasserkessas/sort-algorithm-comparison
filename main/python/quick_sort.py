3f2acc18e2a9136d65dcc89259c6e3494181c81a
Nasser <nasser.kessas@outlook.com>
2022-11-30 12:40:33 +1000

    Added quick sort

---
+++ main/python/quick_sort.py
@@ -0,0 +1,20 @@
+def partition(array, begin, end):
+    pivot = begin
+    for i in range(begin+1, end+1):
+        if array[i] <= array[begin]:
+            pivot += 1
+            array[i], array[pivot] = array[pivot], array[i]
+    array[pivot], array[begin] = array[begin], array[pivot]
+    return pivot
+
+def quick_sort(array, begin=0, end=None):
+    if end is None:
+        end = len(array) - 1
+    def _quick_sort(array, begin, end):
+        if begin >= end:
+            return
+        pivot = partition(array, begin, end)
+        _quick_sort(array, begin, pivot-1)
+        _quick_sort(array, pivot+1, end)
+        return array
+    return _quick_sort(array, begin, end)
