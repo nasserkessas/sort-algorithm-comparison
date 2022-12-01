cff16c989c0c5d407090a55db60bea219b8a7276
Nasser <nasser.kessas@outlook.com>
2022-12-01 16:54:15 +1000

    Added loading, increasing array lengths and writing data to file

--- main/python/test/main.py
+++ main/python/test/main.py
@@ -15,42 +15,51 @@ from hoares_quick_sort import hoares_quick_sort
 from tim_sort import tim_sort
 from heap_sort import heap_sort
 
-algorithms = [bubble_sort, comb_sort, insertion_sort, merge_sort, radix_sort, selection_sort, quick_sort, hoares_quick_sort, tim_sort, heap_sort]
+# algorithms = [bubble_sort, comb_sort, insertion_sort, merge_sort, radix_sort, selection_sort, quick_sort, hoares_quick_sort, tim_sort, heap_sort]
+algorithms = [comb_sort, merge_sort, radix_sort, quick_sort, hoares_quick_sort, tim_sort, heap_sort] # removed slow algorithms for better graph
 max_name_length = (len(max(map(lambda a: a.__name__, algorithms), key=len)))
 
-ARRAY_LENGTH = 100
-TRIALS = 1000
+ARRAY_LENGTH_START = 50
+ARRAY_LENGTH_INCREMENT = 50
+ARRAY_LENGTH_END = 500
+TRIALS = 5000
 
 data = {}
 
 file = open(os.path.join(os.path.dirname(__file__), 'data.json'), "w")
 
-bar = Bar('Sorting', max=TRIALS, suffix="%(percent).1f%% - %(eta)ds remaining")
-for t in range(TRIALS):
-    array = []
+max_array_length_name = len(str(ARRAY_LENGTH_END))
 
-    for n in range(ARRAY_LENGTH):
-        array.append(n+1)
+for length in range(ARRAY_LENGTH_START, ARRAY_LENGTH_END+1, ARRAY_LENGTH_INCREMENT):
+    data[length] = {}
+    bar = Bar(f'Sorting with {length} items{" " * (max_array_length_name - int(len(str(length))))}', max=TRIALS, suffix="%(index)d/%(max)d [%(percent).1f%%] in %(elapsed)ds (eta: %(eta)ds)")
+    for t in range(TRIALS):
+        array = []
 
-    shuffle(array)
+        for n in range(length):
+            array.append(n+1)
 
-    for algorithm in algorithms:
-        if algorithm.__name__.replace('_', ' ') not in data:
-            data[algorithm.__name__.replace('_', ' ')] = []
-        thisArray = [*array]
-        start = datetime.now()
-        sorted = algorithm(thisArray)
-        end = datetime.now()
+        shuffle(array)
 
-        data[algorithm.__name__.replace('_', ' ')].append((end - start).total_seconds() * 10**3)
-    bar.next()
-bar.finish()
+        for algorithm in algorithms:
+            if algorithm.__name__.replace('_', ' ') not in data[length]:
+                data[length][algorithm.__name__.replace('_', ' ')] = []
+            thisArray = [*array]
+            start = datetime.now()
+            sorted = algorithm(thisArray)
+            end = datetime.now()
 
-print(f"\nAverage time to sort an array of {ARRAY_LENGTH} shuffled sequential numbers (tested {TRIALS} times):")
-for a in data:
-    print(f"{a.replace('_', ' ').capitalize()} {' ' * (max_name_length-len(a))} {(sum(data[a])/len(data[a])):.3f}ms")
-    data[a] = list(map(lambda x: round(x,3), data[a]))
+            data[length][algorithm.__name__.replace('_', ' ')].append((end - start).total_seconds() * 10**3)
+        bar.next()
+    bar.finish()
 
+for l in data:
+    for f in data[l]:
+        data[l][f] = list(map(lambda x: round(x,3), data[l][f]))
 
-json.dump(data, file)
-file.close()
+try:
+    json.dump(data, file)
+    file.close()
+    print(f"\nSaved data to {os.path.join(os.path.dirname(__file__), 'data.json')}")
+except:
+    print(f"\nData could not be saved to {os.path.join(os.path.dirname(__file__), 'data.json')}")
