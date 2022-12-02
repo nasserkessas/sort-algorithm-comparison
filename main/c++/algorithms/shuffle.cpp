647a0be3ca982a1bc62fd67e4f001e1aecb94d5f
Nasser <nasser.kessas@outlook.com>
2022-12-02 13:30:53 +1000

    Added shuffle.cpp

---
+++ main/c++/algorithms/shuffle.cpp
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+
+void shuffle(int *array, size_t n) {
+    srand(time(NULL));
+    if (n > 1) {
+        size_t i;
+        for (i = 0; i < n - 1; i++) {
+          size_t j = i + rand() / (RAND_MAX / (n - i) + 1);
+          int t = array[j];
+          array[j] = array[i];
+          array[i] = t;
+        }
+    }
+}
